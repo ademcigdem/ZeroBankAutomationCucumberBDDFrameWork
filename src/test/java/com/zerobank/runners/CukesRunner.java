282a3fb819819c864b2405ae1abe4eb76ec64d38
ademcigdem <adem_cigdem10@hotmail.com>
2020-06-08 12:17:05 +0100

    CukesRunner.java updated and FailedTestRunner.java added

--- src/test/java/com/zerobank/runners/CukesRunner.java
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -6,12 +6,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"json:target/cucumber.json"},
+        plugin = {"json:target/cucumber.json","rerun:target/rerun.txt"},
         features = {"src\\test\\resources\\features"},
         glue = {"com\\zerobank\\stepdefnitions"},
 
         dryRun = false,
-        tags = "@ZB_005"
+        tags = "@outline"
 )
 public class CukesRunner {
 }
